96367fe57f7c722d9ef893636a0cb5e30881f790
VicLo <victoria2666@gmail.com>
2020-07-13 20:37:49 -0400

    Added search bar functions

--- dotabase/src/pages/Search.js
+++ dotabase/src/pages/Search.js
@@ -28,10 +28,20 @@ export default class Search extends React.Component {
             //Assign the promise unresolved first then get the data using the json method. 
             const response = await fetch('https://api.opendota.com/api/heroes');
             const json = await response.json();
-            this.setState({data: json, loaded: true});
+            this.setState({data: json}, this.initArray);
         } catch(err) {
             console.log("Error fetching data", err);
         }
+    }
+    
+    initArray(){
+        this.state.data.some((hero) => {
+            let heroName = hero.name.charAt(14).toUpperCase() + hero.name.slice(15);
+            heroName = heroName.replace(/_/g, " ");
+            this.state.matchedHeroes.push(heroName);
+        }); 
+        this.setState({loaded:true});
+        
     }
 
     fetchHero(search){
@@ -40,7 +50,7 @@ export default class Search extends React.Component {
         this.state.data.some((hero) => {
             if (hero.name.startsWith(search, 14)){
                 let heroName = hero.name.charAt(14).toUpperCase() + hero.name.slice(15);
-                heroName = heroName.replace("_", " ");
+                heroName = heroName.replace(/_/g, " ");
                 this.state.matchedHeroes.push(heroName);
             }
         }); 
@@ -49,7 +59,7 @@ export default class Search extends React.Component {
     render() {
         if(this.state.loaded){
             return(
-                <View>
+                <View style={styles.container}>
                     <Text style={{ marginTop: 50, marginLeft: 15, fontSize: 25 }}>Search Hero</Text>
                     <SearchBar
                         placeholder="Type Here..."
@@ -57,19 +67,14 @@ export default class Search extends React.Component {
                         value={this.state.search}   
                     />
                     <FlatList data={this.state.matchedHeroes} keyExtractor = {(x,i)=>i} renderItem = {({item}) =>
-                    <Text style={{marginLeft: 15}}>{`${item}`}</Text>} />
+                    <Text style={{paddingLeft: 15, marginTop:15, paddingBottom:15, fontSize: 20, borderBottomColor: '#26a69a', borderBottomWidth:1}}>{`${item}`}</Text>} />
                 </View>
                 
             )
         }else{
             return (
                 <View>
-                    <Text style={{ marginTop: 50, marginLeft: 15,fontSize: 25 }}>Loading Hero</Text>
-                    <SearchBar
-                        placeholder="Type Here..."
-                        onChangeText={this.updateSearch}
-                        value={this.state.search}
-                    />
+                    <Text style={{ marginTop: 50, marginLeft: 15,fontSize: 25 }}>Loading...</Text>
                 </View>
             );
         }
@@ -79,8 +84,6 @@ export default class Search extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginBottom: 34
     },
 });
